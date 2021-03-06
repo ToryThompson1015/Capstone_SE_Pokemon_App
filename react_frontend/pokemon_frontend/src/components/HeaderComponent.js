09d7a9fd6df8cd11e3f6a963143a1f92c5a74aab
PSADMIN <tthompson@perscholas.org>
2021-03-05 21:23:12 -0500

    search grabbing proper pokemon data

--- react_frontend/pokemon_frontend/src/components/HeaderComponent.js
+++ react_frontend/pokemon_frontend/src/components/HeaderComponent.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import {useState} from "react";
 import axios from "axios"
 
-const [pokemonName, setPokemonName] = useState("");
 
-class HeaderComponent extends Component {
+function HeaderComponent()  {
+    // set the search bar value to be the pokemonName 
+    const [pokemonName, setPokemonName] = useState("");
+    // function to grab a pokekmon from the api
+    const searchPokemon = () => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+            (res)=> {
+                console.log(res)
+            }
+        )
+    }
+
     
-    render() {
         return (
 
 
@@ -16,14 +25,15 @@ class HeaderComponent extends Component {
                 placeholder="Search for your Fav Pokemon!"
                 onChange={(event) =>{
                   setPokemonName(event.target.value)
+                  console.log(pokemonName)
                 }}>
 
                 </input>
-                <button className="searchButton" >Search</button>
+                <button className="searchButton" onClick={searchPokemon} >Search</button>
             </div>
             
         );
     }
-}
+
 
 export default HeaderComponent;
